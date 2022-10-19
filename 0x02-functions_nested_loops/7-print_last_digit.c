13aba6d683a4c56915c8076f89d37100688c884b
hassanStars95 <seiduhassan36@gmail.com>
2022-10-19 16:30:13 -0700

    8th commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
+
 /**
- * print_last_digit - prints last digits
+ * print_last_digit - prints last digits of given numbers by dividing by 10
  * @b: the character
- * Result: prints the last digit of a number
+ * Return: prints the last digit of a number
  */
 int print_last_digit(int b)
 {
 	int rm = b % 10;
+
 	if (rm < 0)
 		rm *= -1;
 	_putchar (rm + '0');
 	return (0);
+}
