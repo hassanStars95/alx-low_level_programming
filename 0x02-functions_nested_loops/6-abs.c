9614497c4e33aeeec5d280878b61adfb1cdcab6e
hassanStars95 <seiduhassan36@gmail.com>
2022-10-19 15:33:53 -0700

    7th commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - prints absolute numbers
+ * @b: the character
+ * Return: absolute value
+ */
+int _abs(int b)
+{
+	if (b >= 0)
+		return (b);
+	else
+		return (-b);
+}
