decb00c4d16db1a9e12388e02b88aa4edaf51581
hassanStars95 <seiduhassan36@gmail.com>
2022-10-19 03:03:21 -0700

    functions and nested loop first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,16 @@
  * main - prints putchar
  * Return: always 0 (success)
  */
-
 int main(void)
 {
 	_putchar('_');
-	_putchar('p');  
-	_putchar('u');  
-	_putchar('t');  
-	_putchar('c');  
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
-        _putchar('a');
-	_putchar('r');        
-	_putchar('\n');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
